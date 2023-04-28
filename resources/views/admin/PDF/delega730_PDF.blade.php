d3a871a134e256edd1d0ab66f48660d491b06d73
Md. Gofran Khan <gofran0123@gmail.com>
2023-04-28 23:31:49 +0600

    delega 730

--- resources/views/admin/PDF/delega730_PDF.blade.php
+++ resources/views/admin/PDF/delega730_PDF.blade.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <style>
-div {
+div.main {
   margin-top: 20px;
   margin-bottom: 20px;
   margin-right: 5px;
@@ -17,7 +17,7 @@ p, table {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <div>
+    <div class="main">
       <h6 align="center">DELEGA/REVOCA PER L’ACCESSO<br>ALLA DICHIARAZIONE<br>DEI REDDITI PRECOMPILATA <br><br><br><br>IL SOTTOSCRITTO</h6>
       <br>
       <table>
@@ -112,6 +112,35 @@ p, table {
             </tr>
         </table>
       </div>
+      <br>
+      <table>
+            <tr>
+                <td style="width:20px;"><input type="checkbox" checked><td>
+                <td style="width:230px; font-size: 12px;"><b>CONFERISCE DELEGA</b><td>
+                <td style="width:20px;"><input type="checkbox"><td>
+                <td style="width:270px; font-size: 12px;"><b>NON CONFERISCE DELEGA</b><td>
+                <td style="width:20px;"><input type="checkbox"><td>
+                <td style="width:230px; font-size: 12px;"><b>REVOCA DELEGA</b><td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <p style="font-size: 12px;"><b>Al Centro di Assistenza Fiscale CAF UCI SRL - Cod. Fisc./P.Iva 04656741008 - numero di iscrizione all’albo <br> CAF 37 - con sede in ROMA (RM) in VIA IN LUCINA, 10 Cap 00186 - Codice fiscale del responsabile <br>dell’assistenza fiscale del CAF VLNFST67H10H501I</b></p>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td style="width:700px; font-size:12px;">All’accesso e alla consultazione della propria dichiarazione dei redditi precompilata e degli altri dati che l’Agenzia<td>
+            </tr>
+            </table>
+        <table>
+            <tr>
+                <td style="width:560px; font-size: 12px;">delle Entrate mette a disposizione ai fini della compilazione della dichiarazione relativa all’anno d’imposta</td>
+                <td style="width:30px;height:10px;border:1px solid #000;padding-left:2px;padding-right:2px;font-size:10px;">2022</td>
+            </tr>
+        </table>
     </div>
   </body>
 </html>
