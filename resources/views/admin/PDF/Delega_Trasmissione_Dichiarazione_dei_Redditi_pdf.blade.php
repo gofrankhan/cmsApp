84f0c25577fa53fb6cc256c0684b31afafd62e03
Md. Gofran Khan <gofran0123@gmail.com>
2023-06-07 00:52:32 +0600

    new pdf work started

--- resources/views/admin/PDF/Delega_Trasmissione_Dichiarazione_dei_Redditi_pdf.blade.php
+++ resources/views/admin/PDF/Delega_Trasmissione_Dichiarazione_dei_Redditi_pdf.blade.php
@@ -3,23 +3,75 @@
 <html>
 <head>
 <style>
-p, table {
-  font-size: 14px;
+
+div.nwew23 {
+  margin-top: 0px;
+  margin-bottom: 5px;
+  margin-right: 5px;
+  margin-left: 5px;
 }
+
+p {
+  font-size: 10px;
+}
+table, td, tr {
+    font-size : 10px;
+}
+
 </style>
     <title>Delega Trasmissione Dichiarazione dei Redditi pdf</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <div>   
+    <div class="nwew23">   
         <div align="center">
             <img  src="{{ public_path('backend/assets/images/PCPoint_Logo.png') }}" height="100" width="500"  alt="">
         </div>  
         </div>
-        <p align="center">VIA FLAVIO STILCIONE 11, 00175, ROMA (RM)</p>
-        <h6 align="center">Tel- 06 8788 0399, email- <u>cafpcpoint@yahoo.com</u>, pec- <u>cafpcpoint@pec.it</u></h6>
-        <br>
+        <p align="center" style="font-size:12px;">VIA FLAVIO STILCIONE 11, 00175, ROMA (RM)<br>
+        Tel- 06 8788 0399, email- <u>cafpcpoint@yahoo.com</u>, pec- <u>cafpcpoint@pec.it</p>
         <p align="center"><b>DELEGA TRASMISSIONE DICHIARAZIONE DEI REDDITI</b></p>
+
+        <table>
+            <tr>
+                <td style="width:95px;">La/Il sottoscritta/o<td>
+                <td style="width:170px; border-bottom: 0.5px solid black;"></td>
+                <td style="width:43px;"> nata/o il<td>
+                <td style="width:150px; border-bottom: 0.5px solid black;"></td>
+                <td style="width:69px;"> odice fiscale<td>
+                <td style="width:160px; border-bottom: 0.5px solid black;"></td>
+            </tr>
+        </table>
+        <table style="padding-top:10px">
+            <tr>
+                <td style="width:71px;">tel. Cellulare:<td>
+                <td style="width:140px; border-bottom: 0.2px solid black;"></td>
+                <td style="width:60px;">residente a<td>
+                <td style="width:70px; border-bottom: 0.2px solid black;"></td>
+                <td style="width:70px;"> in Via/Piazza<td>
+                <td style="width:150px; border-bottom: 0.2px solid black;"></td>
+                <td style="width:20px;">Prov.<td>
+                <td style="width:30px; border-bottom: 0.2px solid black;"></td>
+                <td style="width:20px;">CAP<td>
+                <td style="width:30px; border-bottom: 0.2px solid black;"></td>
+            </tr>
+        </table>
+        <br>
+        <p align="center"><b>DELEGO</b></p>
+        <table>
+            <tr>
+                <td>"CHAKRABORTY NIBASH TITOLARE CAF PC POINT" con sede legale a ROMA in Via Flavio Stilicone, 11 con p.Iva 15865031007, alla trasmissione della</td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>ichiarazione dei redditi per il l'anno fiscale ANNO</td>
+                <td style="width:40px;"></td>
+                <td>RIF</td>
+                <td style="width:60px;"></td>
+                <td>per il dichiarante con codice fiscale: </td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
