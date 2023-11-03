6498d0f9a304cd2a7b9b1e2a56eac6c71fb824a7
Md. Gofran Khan <gofran0123@gmail.com>
2023-11-03 22:01:54 +0600

    Privacy e GDPR file added

---
+++ resources/views/admin/PDF/flussi1.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+@php
+define("DOMPDF_ENABLE_REMOTE", false);
+@endphp
+<html>
+<head>
+<style>
+p, table {
+  font-size: 16px;
+}
+</style>
+    <title>Privacy e GDPR</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="abcdx"> 
+        
+        <p style="font-size: 19px;" align="center">INFORMATIVA AI SENSI DEGLI ARTT. 13 E 14 DEL REGOLAMENTO EUROPEO N.679/2016</p>
+        <table>
+            <tr>
+                <td style="text-align:justify">Egregio sig. {{ $customer->firstname." ".$customer->lastname }}
+                ai fini previsti dal Regolamento Europeo (UE) n.2016/679 (di seguito GDPR), relativo
+                alla protezione delle persone fisiche con riguardo al trattamento dei dati personali, La informo che il trattamento dei dati
+                personali da Lei forniti ed acquisiti dalla L'ASSOCIAZIONE "UCI ZONALE TUSCOLANO STILICONE"
+                saranno oggetto di trattamento nel rispetto della normativa
+                prevista dal premesso Regolamento, nonché dei diritti ed obblighi conseguenti.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>A tal proposito Le comunichiamo quanto segue:</b><br>
+                Titolare del trattamento e responsabile della protezione dei dati personali - Titolare del trattamento è
+                L'ASSOCIAZIONE "UCI ZONALE TUSCOLANO STILICONE" in
+                persona del legale rappresentante pro tempore (di seguito indicato anche come "L'ASSOCIAZIONE") con sede
+                XXXXXXXXXXXXXX XXXXXXXxXXxxxxxxx xxxxxx x xxxxxxxxx xxxxx xxxx xxxx xxxxx
+                <br>
+                L'ASSOCIAZIONE ha nominato un responsabile della protezione dei dati personali (RPD ovvero, data protection officer, DPO) nella 
+                figura del legale rappresentante pro tempore a cui competono tutti gli obblighi di legge.
+
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Finalità del trattamento dei dati</b><br>
+                Il trattamento è finalizzato alla corretta e completa esecuzione servizio offerto. I suoi dati saranno trattati anche al fine di:
+                 - adempiere agli obblighi previsti in ambito fiscale e contabile; -rispettare gli obblighi incombenti sulla L'ASSOCIAZIONE
+                 e previsti dalla normativa vigente. I dati personali potranno essere trattati a mezzo sia di archivi cartacei che informatici
+                 (ivi compresi dispositivi portatili) e trattati con modalità strettamente necessarie a far fronte alle finalità sopra indicate.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Base giuridica del trattamento</b><br>
+                L'ASSOCIAZIONE tratta i Suoi dati personali lecitamente, laddove il trattamento:<br>
+                -sia necessario all’esecuzione del contratto o all’esecuzione di misure precontrattuali adottate su richiesta;
+                -sia necessario per adempiere un obbligo legale incombente sulla L'ASSOCIAZIONE
+                ; -sia basato sul consenso espresso.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Conseguenze della mancata comunicazione dei dati personali</b><br>
+                Con riguardo ai dati personali relativi all'esecuzione del contratto di cui Lei è parte o relativi all'adempimento ad 
+                un obbligo normativo (ad esempio gli adempimenti legati alla tenuta delle scritture contabili e fiscali),
+                la mancata comunicazione dei dati personali impedisce il perfezionarsi del rapporto contrattuale stesso.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Conservazione dei dati</b><br>
+                I Suoi dati personali, oggetto di trattamento per le finalità sopra indicate, 
+                saranno conservati per il periodo di durata del contratto e, successivamente, per il tempo in cui
+                L'ASSOCIAZIONE sia soggetta a obblighi di conservazione per finalità fiscali o per altre finalità, 
+                previsti, da norme di legge o regolamento.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Comunicazione dei dati</b><br>
+                I Suoi dati personali potranno essere comunicati a: 1. consulenti e commercialisti o altri legali che eroghino prestazioni 
+                funzionali ai fini sopra indicati; 2. istituti bancari e assicurativi che eroghino prestazioni funzionali ai fini sopra indicati; 
+                3. soggetti che elaborano i dati in esecuzione di specifici obblighi di legge; 
+                4. Autorità giudiziarie o amministrative, per l’adempimento degli obblighi di legge.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Profilazione e Diffusione dei dati</b><br>
+                I Suoi dati personali non sono soggetti a diffusione né ad alcun processo decisionale interamente automatizzato, 
+                ivi compresa la profilazione.
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="text-align:justify"><b>Diritti dell’interessato</b><br>
+                Tra i diritti a Lei riconosciuti dal GDPR rientrano quelli di: -chiedere alla L'ASSOCIAZIONE
+                l'accesso ai Suoi dati personali ed alle informazioni relative agli stessi; la rettifica dei dati inesatti o 
+                l'integrazione di quelli incompleti; la cancellazione dei dati personali che La riguardano (al verificarsi di una delle 
+                condizioni indicate nell'art. 17, paragrafo 1 del GDPR e nel rispetto delle eccezioni previste nel paragrafo 
+                3 dello stesso articolo); la limitazione del trattamento dei Suoi dati personali 
+                (al ricorrere di una delle ipotesi indicate nell'art. 18, paragrafo 1 del GDPR); -richiedere ed ottenere dalla L'ASSOCIAZIONE
+                - nelle ipotesi in cui la base giuridica del trattamento sia il contratto o il consenso, e lo stesso sia effettuato con 
+                mezzi automatizzati - i Suoi dati personali in un formato strutturato e leggibile da dispositivo automatico, anche al fine di 
+                comunicare tali dati ad un altro titolare del trattamento (c.d. diritto alla portabilità dei dati personali); 
+                -opporsi in qualsiasi momento al trattamento dei Suoi dati personali al ricorrere di situazioni particolari che La riguardano; 
+                -revocare il consenso in qualsiasi momento, limitatamente alle ipotesi in cui il trattamento sia basato sul Suo consenso per 
+                una o più specifiche finalità e riguardi dati personali comuni (ad esempio data e luogo di nascita o luogo di residenza), 
+                oppure particolari categorie di dati (ad esempio dati che rivelano la Sua origine razziale, le Sue opinioni politiche, 
+                le Sue convinzioni religiose, lo stato di salute o la vita sessuale). Il trattamento basato sul consenso ed effettuato 
+                antecedentemente alla revoca dello stesso conserva, comunque, la sua liceità; -proporre reclamo a un'autorità di controllo 
+                (Autorità Garante per la protezione dei dati personali – 
+                www.garanteprivacy.it). Il/la sottoscritto/a dichiara di aver ricevuto l’informativa che precede.
+                </td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td>Luogo e data {{ $customer->city }} ___/___/______</td>
+                <td style="width:240px"> </td>
+                <td align="right">Firma _________________</td>
+            </tr>
+        </table>
+        <h5 align="center">CONSENSO AL TRATTAMENTO DEI DATI PERSONALI</h5>
+        @php
+            if($customer->dateofbirth == '0000-00-00')
+            {
+                $date1="00";
+                $month1="00";
+                $year1="0000";
+            }else{
+                $time=strtotime($customer->dateofbirth);
+                $date1=date("d",$time);
+                $month1=date("m",$time);
+                $year1=date("Y",$time);
+            }
+        @endphp
+        <table>
+            <tr>
+                <td style="text-align:justify">ILa/Il sottoscritta/o
+                {{ $customer->firstname." ".$customer->lastname }} nata/o il {{ $date1."/".$month1."/".$year1; }} codice fiscale {{ $customer->taxid }}
+                tel. Cellulare: {{ $customer->mobile }} residente a {{ $customer->city }} in Via {{ $customer->addressline1 }}
+                Prov. {{ $customer->postcode }} CAP
+                @if(!empty($customer->region)){{ $customer->region[0].$customer->region[1] }}@endif
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>essendo stata informato:</td>
+            </tr>
+            <tr>
+                <td>-dell’identità del titolare del trattamento dei dati</td>
+            </tr>
+            <tr>
+                <td>-dell’identità del Responsabile della protezione dei dati</td>
+            </tr>
+            <tr>
+                <td>-della misura/modalità con le quali il trattamento avviene</td>
+            </tr>
+            <tr>
+                <td>-delle finalità del trattamento cui sono destinati i dati personali</td>
+            </tr>
+            <tr>
+                <td>-del diritto alla revoca del consenso</td>
+            </tr>
+            <tr>
+                <td style="text-align:justify">così come indicato dalle lettere a, b, c, d, e, f, h dell’informativa sottoscritta ai sensi dell’art.13 del 
+                Regolamento UE 2016/679, con la sottoscrizione del presente modulo</td>
+            </tr>
+        </table>
+        <br>
+        <h6 align="center">ACCONSENTE</h6>
+        <br>
+        <table>
+            <tr>
+                <td style="text-align:justify">ai sensi e per gli effetti dell’art.7 e ss. del Regolamento UE 2016/679, al trattamento dei dati personali 
+                    secondo le modalità e nei limiti di cui all’informativa che precede. Letto, confermato e sottoscritto</td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>Luogo e data {{ $customer->city }} ___/___/______</td>
+                <td style="width:240px"> </td>
+                <td align="right">Firma _________________</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
